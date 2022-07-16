2638c77794f91e137c8fda47e5c41046569a423f
Derek Visch <dvisch@autoidm.com>
2022-07-16 19:35:41 -0400

    Lint passing, CI should pass!

--- target_postgres/sinks.py
+++ target_postgres/sinks.py
@@ -1,10 +1,10 @@
 """Postgres target sink class, which handles writing streams."""
 from singer_sdk.sinks import SQLSink
+
 from target_postgres.connector import PostgresConnector
 
+
 class PostgresSink(SQLSink):
-    connector_class = PostgresConnector
     """Postgres target sink class."""
-    
 
-
+    connector_class = PostgresConnector
--- target_postgres/tests/test_core.py
+++ target_postgres/tests/test_core.py
@@ -1,8 +1,5 @@
 """Tests standard target features using the built-in SDK tests library."""
-
-import datetime
-
-from typing import Dict, Any
+from typing import Any, Dict
 
 from singer_sdk.testing import get_standard_target_tests
 
