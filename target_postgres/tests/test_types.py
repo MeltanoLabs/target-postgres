0e6eb1f50e26aa075622008920424445b867b9e5
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2026-05-14 15:08:27 -0600

    feat: Support `pgvector` as a custom `x-sql-datatype` (#587)
    - https://github.com/meltano/sdk/pull/3373 for `dim` support
    
    Signed-off-by: Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- target_postgres/tests/test_types.py
+++ target_postgres/tests/test_types.py
@@ -1,6 +1,7 @@
 """Test custom types and the type hierarchy."""
 
 import pytest
+from pgvector.sqlalchemy import VECTOR
 from sqlalchemy import types
 from sqlalchemy.dialects.postgresql import BIGINT, SMALLINT
 
@@ -66,6 +67,17 @@ class TestJSONSchemaToPostgres:
     def to_postgres(self, connector: PostgresConnector):
         """Create a JSONSchemaToPostgres instance."""
         return connector.jsonschema_to_sql
+
+    @pytest.fixture
+    def base_vector_schema(self):
+        """Create a base vector schema."""
+        return {
+            "type": "array",
+            "items": {
+                "type": "number",
+            },
+            "x-sql-datatype": "pgvector",
+        }
 
     def test_datetime_string(self, to_postgres: JSONSchemaToPostgres):
         """Test conversion of JSON schema string to Postgres datetime."""
@@ -218,3 +230,47 @@ class TestJSONSchemaToPostgres:
             {"type": ["string", "null"], "format": "date-time"}
         )
         assert type(result) is types.TIMESTAMP
+
+    def test_vector_no_dimension(
+        self,
+        to_postgres: JSONSchemaToPostgres,
+        base_vector_schema: dict,
+        subtests: pytest.Subtests,
+        monkeypatch: pytest.MonkeyPatch,
+    ):
+        """Test conversion of to pgvector without dimension specified."""
+        result = to_postgres.to_sql_type({**base_vector_schema})
+        assert isinstance(result, VECTOR)
+        assert result.dim is None
+
+    @pytest.mark.xfail(reason="Dimension is not supported yet.", strict=True)
+    def test_vector_with_dimension(
+        self,
+        to_postgres: JSONSchemaToPostgres,
+        base_vector_schema: dict,
+        subtests: pytest.Subtests,
+        monkeypatch: pytest.MonkeyPatch,
+    ):
+        """Test conversion of to pgvector with dimension specified."""
+        dim = 10
+        result = to_postgres.to_sql_type(
+            {
+                **base_vector_schema,
+                "x-sql-datatype-properties": {"dim": dim},
+            }
+        )
+        assert isinstance(result, VECTOR)
+        assert result.dim == dim
+
+    def test_vector_library_not_available(
+        self,
+        to_postgres: JSONSchemaToPostgres,
+        base_vector_schema: dict,
+        subtests: pytest.Subtests,
+        monkeypatch: pytest.MonkeyPatch,
+    ):
+        """Test conversion of to pgvector with library not available."""
+        monkeypatch.setattr("target_postgres.connector.PGVECTOR_AVAILABLE", False)
+        result = to_postgres.to_sql_type({**base_vector_schema})
+        assert isinstance(result, types.ARRAY)
+        assert isinstance(result.item_type, types.INTEGER)
